2fe743a627836b09c49bf80c25c6c4d8cace730e
Matías Agustín Benítez <matiasagbenitez@gmail.com>
2024-05-22 11:31:44 -0300

    entity account creation with formik

--- frontend/src/app/pages/entities/EntityAccounts.tsx
+++ frontend/src/app/pages/entities/EntityAccounts.tsx
@@ -1,17 +1,24 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { Button, Row, Col, Modal, Form, ListGroup } from "react-bootstrap";
+import {
+  Button,
+  Row,
+  Col,
+  Modal,
+  ListGroup,
+  ButtonGroup,
+} from "react-bootstrap";
 
 import apiSJM from "../../../api/apiSJM";
-import { LoadingSpinner, PageHeader } from "../../components";
+import { CustomInput, LoadingSpinner, PageHeader } from "../../components";
 import { NumberFormatter } from "../../helpers";
 import { SweetAlert2 } from "../../utils";
 
 import { EntityAccountInterface, EntityBasicInfoInterface } from "./interfaces";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
 
-const initialForm = {
-  id_currency: "",
-};
+const initialForm = { id_currency: "" };
 
 export const EntityAccounts = () => {
   const { id } = useParams();
@@ -22,7 +29,6 @@ export const EntityAccounts = () => {
   const [accounts, setAccounts] = useState<EntityAccountInterface[]>([]);
   const [currencies, setCurrencies] = useState([]);
   const [showModal, setShowModal] = useState(false);
-  const [form, setForm] = useState(initialForm);
   const [isFormSubmitting, setIsFormSubmitting] = useState(false);
 
   const fetch = async () => {
@@ -52,20 +58,20 @@ export const EntityAccounts = () => {
 
   const handleClose = () => {
     setShowModal(false);
-    setForm(initialForm);
   };
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const handleSubmit = async (values: { id_currency: string }) => {
     try {
-      setIsFormSubmitting(true);
       const confirmation = await SweetAlert2.confirm(
-        "¿Estás seguro de crear una nueva cuenta corriente?"
+        "¿Crear una nueva cuenta corriente?"
       );
       if (confirmation.isConfirmed) {
-        const formData = { ...form, id_entity: id };
-        const { data } = await apiSJM.post("/entity_accounts", formData);
-        SweetAlert2.successToast(data.message || "¡Cuenta creada!");
+        setIsFormSubmitting(true);
+        const { data } = await apiSJM.post("/entity_accounts", {
+          id_entity: id,
+          id_currency: values.id_currency,
+        });
+        SweetAlert2.successToast(data.message || "¡Operación exitosa!");
         fetch();
         handleClose();
       }
@@ -153,44 +159,54 @@ export const EntityAccounts = () => {
 
           <Modal show={showModal} onHide={handleClose}>
             <Modal.Header closeButton>
-              <Modal.Title className="fs-5">Crear cuenta corriente</Modal.Title>
+              <Modal.Title>Nueva cuenta corriente</Modal.Title>
             </Modal.Header>
-            <Modal.Body>
-              <Form onSubmit={handleSubmit}>
-                <label className="form-label">Moneda de la cuenta</label>
-                <Form.Select
-                  name="id_currency"
-                  size="sm"
-                  required
-                  onChange={(e) =>
-                    setForm({ ...form, id_currency: e.target.value })
-                  }
-                  disabled={isFormSubmitting}
-                >
-                  <option value="">Seleccione una moneda</option>
-                  {currencies.map((currency: any) => (
-                    <option key={currency.id} value={currency.id}>
-                      {currency.name}
-                    </option>
-                  ))}
-                </Form.Select>
+            <Formik
+              initialValues={initialForm}
+              onSubmit={(values) => {
+                handleSubmit(values);
+              }}
+              validationSchema={Yup.object({
+                id_currency: Yup.string().required("La moneda es requerida"),
+              })}
+            >
+              {({ errors, touched }) => (
+                <Form id="form">
+                  <Modal.Body>
+                    <CustomInput.Select
+                      label="Moneda de la cuenta corriente"
+                      name="id_currency"
+                      isInvalid={!!errors.id_currency && touched.id_currency}
+                      disabled={isFormSubmitting}
+                      isRequired
+                    >
+                      <option value="">Seleccione una moneda</option>
+                      {currencies.map((currency: any) => (
+                        <option key={currency.id} value={currency.id}>
+                          {currency.name}
+                        </option>
+                      ))}
+                    </CustomInput.Select>
+                  </Modal.Body>
 
-                <div className="d-flex justify-content-end mt-3 gap-2">
-                  <Button size="sm" variant="secondary">
-                    Cancelar
-                  </Button>
-                  <Button
-                    type="submit"
-                    size="sm"
-                    variant="primary"
-                    disabled={isFormSubmitting}
-                  >
-                    <i className="bi bi-floppy me-2"></i>
-                    Guardar
-                  </Button>
-                </div>
-              </Form>
-            </Modal.Body>
+                  <Modal.Footer>
+                    <ButtonGroup size="sm">
+                      <Button variant="secondary" disabled={isFormSubmitting}>
+                        Cancelar
+                      </Button>
+                      <Button
+                        variant="primary"
+                        type="submit"
+                        disabled={isFormSubmitting}
+                      >
+                        <i className="bi bi-floppy mx-1"></i>{" "}
+                        {isFormSubmitting ? "Guardando..." : "Guardar"}
+                      </Button>
+                    </ButtonGroup>
+                  </Modal.Footer>
+                </Form>
+              )}
+            </Formik>
           </Modal>
         </>
       )}
